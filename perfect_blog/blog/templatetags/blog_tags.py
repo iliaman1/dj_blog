769acb689bfe45d6ec59b0f1111f58309c688167
ilia <iliaman1993@gmail.com>
2023-09-08 01:42:52 +0300

    Added inclusive tags to display categories in the sidebar

---
+++ perfect_blog/blog/templatetags/blog_tags.py
@@ -0,0 +1,16 @@
+from django import template
+from blog.models import Category
+
+register = template.Library()
+
+
+@register.simple_tag(name='getcats')
+def get_categories(filter=None):
+    if not filter:
+        return Category.objects.all()
+    return Category.objects.filter(pk=filter)
+
+
+@register.inclusion_tag('blog/list_categories.html')
+def show_categories(cat_selected):
+    return {'categories': Category.objects.all(), 'cat_selected': cat_selected}

cf674f3e957f8bb76c8b10eb85c2666e0e5da775
ilia <iliaman1993@gmail.com>
2023-09-07 19:18:39 +0300

    Changed the import of views to human ones, added paths for views.

--- perfect_blog/blog/urls.py
+++ perfect_blog/blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import *
+from .views import index, about, addpost, login, show_post
 
 urlpatterns = [
     path('', index, name='home'),
-    path('about/', about, name='about')
+    path('about/', about, name='about'),
+    path('addpost/', addpost, name='addpost'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>', show_post, name='post')
 ]

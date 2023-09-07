ae17c3ea961fd4d5215cda06f147a07c6e3c1339
ilia <iliaman1993@gmail.com>
2023-09-08 01:52:01 +0300

    Added paths for viewing a list of posts by category and a path for viewing a post (stub)

--- perfect_blog/blog/urls.py
+++ perfect_blog/blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import index, about, addpost, login, show_post
+from .views import index, about, addpost, login, show_post, show_category
 
 urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
     path('addpost/', addpost, name='addpost'),
     path('login/', login, name='login'),
-    path('post/<int:post_id>', show_post, name='post')
+    path('post/<int:post_id>', show_post, name='post'),
+    path('category/<int:category_id>', show_category, name='category')
 ]

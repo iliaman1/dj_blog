fefc4ee2e15b76cf1286dd45630ff70f6ad700f4
ilia <iliaman1993@gmail.com>
2023-09-07 19:17:30 +0300

    I changed the structure of the navigation menu, in the index view function I pass data to the template through the context, I added stub functions.

--- perfect_blog/blog/views.py
+++ perfect_blog/blog/views.py
@@ -2,21 +2,33 @@ from django.shortcuts import render
 
 from .models import Post
 
-nav_menu = ['Создать пост', 'О всяком', 'Войти']
+nav_menu = [
+    {'title': 'Создать пост', 'url_name': 'addpost'},
+    {'title': 'О всяком', 'url_name': 'about'},
+    {'title': 'Войти', 'url_name': 'login'}
+]
 
 
 def index(request):
     posts = Post.objects.all()
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-            'nav_menu': nav_menu,
-            'title': 'Main page'
-        }
-    )
+    context = {
+        'posts': posts,
+        'nav_menu': nav_menu,
+        'title': 'Main page'
+    }
+    return render(request, 'blog/index.html', context=context)
 
 
 def about(request):
     return render(request, 'blog/about.html', {'nav_menu': nav_menu, 'title': 'О всяком'})
+
+
+def addpost(request):
+    ...
+
+
+def login(request):
+    ...
+
+def show_post(request, post_id):
+    ...

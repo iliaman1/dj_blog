cde64723e036eddac3612c871d6f8fae44482d8d
ilia <iliaman1993@gmail.com>
2023-09-08 18:01:05 +0300

    Added slug in models

--- perfect_blog/blog/models.py
+++ perfect_blog/blog/models.py
@@ -4,12 +4,13 @@ from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=64, db_index=True, verbose_name='Категория')
+    slug = models.SlugField(max_length=128, unique=True, db_index=True, verbose_name='URL')
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('category', kwargs={'category_id': self.pk})
+        return reverse('category', kwargs={'category_slug': self.slug})
 
     class Meta:
         verbose_name = 'Категория'
@@ -19,6 +20,7 @@ class Category(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=128, verbose_name='Заголовок')
+    slug = models.SlugField(max_length=128, unique=True, db_index=True, verbose_name='URL')
     content = models.TextField(blank=True, verbose_name='Текст поста')
     time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
@@ -29,7 +31,7 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post', kwargs={'post_id': self.pk})
+        return reverse('post', kwargs={'post_slug': self.slug})
 
     class Meta:
         verbose_name = 'Пост'

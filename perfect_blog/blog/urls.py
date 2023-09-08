2812e061ebf82c67e8d36fe5f13b6a736759b362
ilia <iliaman1993@gmail.com>
2023-09-08 18:02:08 +0300

    changed routes using slug

--- perfect_blog/blog/urls.py
+++ perfect_blog/blog/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('about/', about, name='about'),
     path('addpost/', addpost, name='addpost'),
     path('login/', login, name='login'),
-    path('post/<int:post_id>', show_post, name='post'),
-    path('category/<int:category_id>', show_category, name='category')
+    path('post/<slug:post_slug>', show_post, name='post'),
+    path('category/<slug:category_slug>', show_category, name='category')
 ]

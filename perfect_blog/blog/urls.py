d0c9a7f7e9778e29a4e98089650a5546f5ab4179
ilia <iliaman1993@gmail.com>
2023-09-09 12:06:23 +0300

    reworked to CBV(Class-Based Views)

--- perfect_blog/blog/urls.py
+++ perfect_blog/blog/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from .views import index, about, addpost, login, show_post, show_category
+from .views import BlogHome, BlogCategory, ShowPost, AddPost, about, login
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', BlogHome.as_view(), name='home'),
+    path('category/<slug:category_slug>', BlogCategory.as_view(), name='category'),
+    path('post/<slug:post_slug>', ShowPost.as_view(), name='post'),
+    path('addpost/', AddPost.as_view(), name='addpost'),
     path('about/', about, name='about'),
-    path('addpost/', addpost, name='addpost'),
-    path('login/', login, name='login'),
-    path('post/<slug:post_slug>', show_post, name='post'),
-    path('category/<slug:category_slug>', show_category, name='category')
+
+    path('login/', login, name='login')
+
 ]
--- perfect_blog/blog/views.py
+++ perfect_blog/blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+
 from .models import Post, Category
 from .forms import AddPostForm
 
@@ -9,56 +12,68 @@ nav_menu = [
 ]
 
 
-def index(request):
-    posts = Post.objects.all()
-    context = {
-        'posts': posts,
-        'nav_menu': nav_menu,
-        'title': 'Все посты',
-        'cat_selected': 0,
-    }
-    return render(request, 'blog/index.html', context=context)
+class BlogHome(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['nav_menu'] = nav_menu
+        context['title'] = 'Все посты'
+        context['cat_selected'] = 0
+        return context
+
+    def get_queryset(self):
+        return Post.objects.filter(is_published=True)
+
+
+class BlogCategory(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['nav_menu'] = nav_menu
+        context['title'] = context['posts'][0].category
+        context['cat_selected'] = context['posts'][0].category_id
+        return context
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs['category_slug'], is_published=True)
+
+
+class ShowPost(DetailView):
+    model = Post
+    template_name = 'blog/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['nav_menu'] = nav_menu
+        context['title'] = context['post'].title
+        context['cat_selected'] = context['post'].category_id
+        return context
+
+
+class AddPost(CreateView):
+    form_class = AddPostForm
+    template_name = 'blog/addpost.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['nav_menu'] = nav_menu
+        context['title'] = 'Создание поста'
+        return context
 
 
 def about(request):
     return render(request, 'blog/about.html', {'nav_menu': nav_menu, 'title': 'О всяком'})
 
 
-def addpost(request):
-    if request.method == 'POST':
-        form = AddPostForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = AddPostForm()
-    context = {'form': form, 'nav_menu': nav_menu, 'title': 'Добавить пост'}
-    return render(request, 'blog/addpost.html', context=context)
-
-
 def login(request):
     ...
-
-
-def show_post(request, post_slug):
-    post = get_object_or_404(Post, slug=post_slug)
-
-    context = {
-        'post': post,
-        'nav_menu': nav_menu,
-        'title': post.title,
-        'cat_selected': post.category_id,
-    }
-    return render(request, 'blog/post.html', context=context)
-
-
-def show_category(request, category_slug):
-    category = Category.objects.get(slug=category_slug)
-    posts = Post.objects.filter(category=category.pk)
-    context = {
-        'posts': posts,
-        'nav_menu': nav_menu,
-        'title': f'категория {category}',
-        'cat_selected': category.pk,
-    }
-    return render(request, 'blog/index.html', context=context)

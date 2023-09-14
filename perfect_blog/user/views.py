2e533a915206b6a883a9bf3502e060a2baff08b4
ilia <iliaman1993@gmail.com>
2023-09-14 15:14:11 +0300

    added views for login & logout

--- perfect_blog/user/views.py
+++ perfect_blog/user/views.py
@@ -1,14 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth import logout, login
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from .forms import RegistrationForm
+from django.contrib.auth.views import LoginView
+from .forms import RegistrationForm, LoginForm
 
 
 class Registration(CreateView):
     form_class = RegistrationForm
     template_name = 'user/registration.html'
-    success_url = reverse_lazy('home')
+    # success_url = reverse_lazy('home')
     raise_exception = False
-    additional_context = {
+    extra_context = {
         'title': 'Регистрация'
     }
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+class Login(LoginView):
+    form_class = LoginForm
+    template_name = 'user/login.html'
+    extra_context = {
+        'title': 'Авторизация'
+    }
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')

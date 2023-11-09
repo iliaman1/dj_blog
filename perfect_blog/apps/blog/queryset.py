f074091d84aa642461cc9daa179d43ddbc855eef
iliaman <iliaman1993@gmail.com>
2023-11-09 14:40:32 +0300

    Added custom queriset: Rating, Best for the period

--- perfect_blog/apps/blog/queryset.py
+++ perfect_blog/apps/blog/queryset.py
@@ -1,18 +1,42 @@
+from datetime import timedelta
+
 from django.db import models
-from django.db.models import F
+from django.db.models import F, Q
 from django.db.models import Count
+from django.utils import timezone
 
 
 class PostQuerySet(models.QuerySet):
-    def is_published(self):
-        return self.filter(is_published=True)
-
-    def annotate_rating(self):
-        return self.annotate(
-            number_of_likes=Count("likes"),
-            number_of_dislikes=Count("dislikes"),
-            rating=F('number_of_likes') - F('number_of_dislikes')
-        )
 
     def is_author(self, user):
         return self.filter(author=user.pk)
+
+    def is_published(self):
+        return self.filter(is_published=True)
+
+    def annotate_rating(self, period: Q = Q()):
+        """
+        Вычисляем рейтинг для постов за определенный период,
+        по умолчанию для всего периода.
+        """
+
+        return self.annotate(
+            upvotes=Count("votes", filter=Q(vote__positive=True) & period),
+            downvotes=Count("votes", filter=Q(vote__positive=False) & period),
+            rating=F("upvotes") - F("downvotes")
+        )
+
+    def best_of_period(self, period: timedelta):
+        assert isinstance(period, timedelta), 'period should be timedelta instance'
+
+        now = timezone.now()
+
+        return (
+            self.filter(
+                created_at__gte=now - period
+            )
+            .annotate_rating(
+                Q(vote__created_at__gte=now - period)
+            )
+            .order_by('-rating')
+        )

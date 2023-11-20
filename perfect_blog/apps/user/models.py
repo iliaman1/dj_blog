e4e3afd39f3a01896093f76b242b074a0ab322b8
iliaman <iliaman1993@gmail.com>
2023-11-20 14:00:07 +0300

    Change profile model

--- perfect_blog/apps/user/models.py
+++ perfect_blog/apps/user/models.py
@@ -1,20 +1,25 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import gettext as _
+from django.urls import reverse
+
+from core.models import BaseModel
 
 
-class Profile(models.Model):
+class Profile(BaseModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    username = models.CharField(max_length=50, blank=True, null=True)
-    full_name = models.CharField(max_length=50, blank=True, null=True)
+    name = models.CharField(unique=True, max_length=50, blank=True, null=True)
+    avatar = models.ImageField(null=True, blank=True, upload_to="images/profile/%Y/%m/%d/")
+    bio = models.TextField(null=True, blank=True)
     email = models.EmailField(max_length=50, blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Время создания'))
-    update_at = models.DateTimeField(auto_now=True, verbose_name=_('Время изменения'))
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('showprofile', kwargs={'name': self.name})
 
     class Meta:
         ordering = ['created_at']
         verbose_name = _('Профиль')
         verbose_name_plural = _('Профили')
-
-    def __str__(self):
-        return self.user.username

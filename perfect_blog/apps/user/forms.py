87fb94f01cfa0a63e85db32c0120b86bcd4683e1
iliaman <iliaman1993@gmail.com>
2023-11-20 13:57:20 +0300

    Form for edit profile

--- perfect_blog/apps/user/forms.py
+++ perfect_blog/apps/user/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class RegistrationForm(UserCreationForm):
@@ -12,3 +13,15 @@ class RegistrationForm(UserCreationForm):
 class LoginForm(AuthenticationForm):
     username = forms.CharField(label='Логин', widget=forms.TextInput())
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput())
+
+
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'avatar', 'bio', 'email']
+
+
+class EditUserForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']

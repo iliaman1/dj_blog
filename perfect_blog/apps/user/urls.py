66d1727d9a931a2a8251539ea1a6fef7a7b05341
iliaman <iliaman1993@gmail.com>
2023-11-20 16:53:09 +0300

    Added the ability to view user posts, likes and comments to the profile. The profile owner can change the data in his profile.

--- perfect_blog/apps/user/urls.py
+++ perfect_blog/apps/user/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 
-from .views import Registration, Login, logout_user, ShowProfile, EditProfile
+from .views import Registration, Login, logout_user, ShowProfile, EditProfile, ShowUserPosts, ShowUserComments, \
+    ShowUserVotes
 
 urlpatterns = [
     path('registration/', Registration.as_view(), name='registration'),
     path('login/', Login.as_view(), name='login'),
     path('logout/', logout_user, name='logout'),
     path('profile/<str:name>', ShowProfile.as_view(), name='showprofile'),
+    path('profile/<str:name>/posts', ShowUserPosts.as_view(), name='showuserposts'),
+    path('profile/<str:name>/comments', ShowUserComments.as_view(), name='showusercomments'),
+    path('profile/<str:name>/votes', ShowUserVotes.as_view(), name='showuservotes'),
     path('editprofile/<str:name>', EditProfile.as_view(), name='editprofile'),
 ]
--- perfect_blog/apps/user/views.py
+++ perfect_blog/apps/user/views.py
@@ -8,6 +8,8 @@ from .models import Profile
 from .forms import RegistrationForm, LoginForm, EditProfileForm
 from core.utils import BaseDataMixin
 
+from apps.blog.models import Post
+
 
 class ShowProfile(BaseDataMixin, DetailView):
     model = Profile
@@ -20,6 +22,60 @@ class ShowProfile(BaseDataMixin, DetailView):
         page_user = get_object_or_404(Profile, name=self.kwargs['name'])
         context = super().get_context_data()
         context['page_user'] = page_user
+        return context
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Profile, name=self.kwargs['name'])
+
+
+class ShowUserPosts(BaseDataMixin, DetailView):
+    model = Profile
+    template_name = 'user/user_profile.html'
+    extra_context = {
+        'title': 'Профиль'
+    }
+
+    def get_context_data(self, *args, **kwargs):
+        page_user = get_object_or_404(Profile, name=self.kwargs['name'])
+        context = super().get_context_data()
+        context['page_user'] = page_user
+        context['posts'] = Post.objects.filter(author__profile__name=self.kwargs['name'])
+        return context
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Profile, name=self.kwargs['name'])
+
+
+class ShowUserComments(BaseDataMixin, DetailView):
+    model = Profile
+    template_name = 'user/user_profile.html'
+    extra_context = {
+        'title': 'Профиль'
+    }
+
+    def get_context_data(self, *args, **kwargs):
+        page_user = get_object_or_404(Profile, name=self.kwargs['name'])
+        context = super().get_context_data()
+        context['page_user'] = page_user
+        context['posts'] = Post.objects.filter(comments__owner__profile__name=self.kwargs['name'])
+        return context
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Profile, name=self.kwargs['name'])
+
+
+class ShowUserVotes(BaseDataMixin, DetailView):
+    model = Profile
+    template_name = 'user/user_profile.html'
+    extra_context = {
+        'title': 'Профиль'
+    }
+
+    def get_context_data(self, *args, **kwargs):
+        page_user = get_object_or_404(Profile, name=self.kwargs['name'])
+        context = super().get_context_data()
+        context['page_user'] = page_user
+        context['posts'] = Post.objects.filter(vote__user__profile__name=self.kwargs['name'])
         return context
 
     def get_object(self, queryset=None):
